67bac031b6f2aa40cd6c71656352b9dfee8ec194
janoantelo7 <jano.anteloabelenda@gmail.com>
2026-04-21 12:40:18 +0200

    feat: add cmp and lsp with ruff and basedpyright for python dev

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,15 +3,71 @@
 -- ========================================================================== 
 
 vim.pack.add({
-	{src = "https://github.com/folke/tokyonight.nvim", name = "tokyonight" }
+    {src = "https://github.com/folke/tokyonight.nvim", name = "tokyonight" },
+    {src = "https://github.com/neovim/nvim-lspconfig" }, -- Language Server
+    -- Completion engine and sources
+    {src = "https://github.com/hrsh7th/nvim-cmp" },
+    {src = "https://github.com/hrsh7th/cmp-nvim-lsp" },
+    {src = "https://github.com/hrsh7th/cmp-buffer" },
+    {src = "https://github.com/hrsh7th/cmp-path" }
+
+})
+
+-- ==========================================================================
+-- LSP & COMPLETION CONFIG
+-- ========================================================================== 
+
+local cmp = require('cmp')
+
+cmp.setup({
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accep selected item with 'Enter' key
+    }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' }, -- Language Server suggestions
+        { name = 'path' }, -- File system paths
+        { name = 'buffer' }, -- Text from current file
+    })
+})
+
+-- Bridge between nvim-cmp and LSP capabilities
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+vim.lsp.config('basedpyright', {
+    capabilities = capabilities,
+    settings = {
+	python = {
+	    pythonPath = ".venv/bin/python",
+	}
+    }
+})
+
+-- Setup ruff
+vim.lsp.config('ruff', {
+    capabilities = capabilities
+})
+
+-- Enable both servers globally for the current session
+vim.lsp.enable({'basedpyright', 'ruff' })
+
+-- Auto-format Python files on save with ruff 
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*.py",
+    callback = function()
+        vim.lsp.buf.format({ async = false })
+    end,
 })
 
 -- ==========================================================================
 -- CONFIG
 -- ========================================================================== 
 
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.shiftwidth = 4
+vim.opt.number = true -- Show line numbers
+vim.opt.relativenumber = true -- Show relative numbers for easier jumping
+vim.opt.shiftwidth = 4 -- Number of spaces for indentation
 
+-- Apply the color theme
 vim.cmd.colorscheme "tokyonight-storm"
